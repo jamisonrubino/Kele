d3aa48ac5512d9f5c6ffa1d61bfae59b48aa2c7f
Jamison Rubino <jamison.rubino@gmail.com>
2017-08-17 03:03:19 +0000

    Checkpoint 3 complete.

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,9 +3,14 @@ require 'httparty'
 class Kele
     include HTTParty
     
-    def initialize(username, password)
-        response = self.class.post 'https://www.bloc.io/api/v1/sessions', body: {email: username, password: password}
+    def initialize(email, password)
+        response = self.class.post 'https://www.bloc.io/api/v1/sessions', body: {email: email, password: password}
         raise "Invalid username or password" if response.code != 200
         @auth_token = response["auth_token"]
+    end
+    
+    def get_me
+        response = self.class.get 'https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token }
+        current_user = JSON.parse(response.body)
     end
 end
