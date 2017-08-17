ac637d78480bd829b6b85f5e4d907aa6c1980cb2
Jamison Rubino <jamison.rubino@gmail.com>
2017-08-17 18:59:06 +0000

    Checkpoint 5 in progress. get_checkpoit raises 403 error.

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,4 +21,17 @@ class Kele
         JSON.parse(response.body).map { |m| mentor_availability << {"starts_at": m["starts_at"], "ends_at": m["ends_at"]} }
         mentor_availability
     end
+    
+    def get_roadmap(roadmap_id)
+        url = "https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}"
+        response = self.class.get url
+        JSON.parse(response.body)
+    end
+    
+    def get_checkpoint(checkpoint_id)
+        url = "https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}" 
+        response = self.class.get url, headers: { "authorization" => @auth_token }
+        raise "Invalid username or password" if response.code != 200
+        JSON.parse(response.body)
+    end
 end
